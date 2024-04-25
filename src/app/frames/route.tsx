da30a3c882ab3971329de3be75eb09fe75683494
flick <github@0xflick.xyz>
2024-04-25 11:50:28 -0600

    error

--- src/app/frames/route.tsx
+++ src/app/frames/route.tsx
@@ -4,15 +4,58 @@ import { frames } from "./frames";
 import { fetchCast } from "@/neynar/cast";
 
 const handleRequest = frames(async (ctx) => {
-  const messageHash = ctx.message?.castId?.hash;
+  try {
+    const messageHash = ctx.message?.castId?.hash;
 
-  if (!messageHash) {
+    if (!messageHash) {
+      return {
+        image: (
+          <span>
+            To use make it a quote, drop the link in the reply to a cast
+          </span>
+        ),
+        buttons: [
+          <Button
+            action="link"
+            target={`${
+              process.env.VERCEL_URL
+                ? `https://{process.env.VERCEL_URL}`
+                : "http://localhost:3000"
+            }`}
+          >
+            get link
+          </Button>,
+          <Button action="link" target="https://warpcast.com/flick">
+            created by flick
+          </Button>,
+        ],
+      };
+    }
+    const { cast } = await fetchCast({
+      identifier: messageHash,
+      type: "hash",
+    });
+    const parentHash = cast?.parent_hash;
+    const ogImage = `${
+      process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000"
+    }/quote/${parentHash}`;
     return {
-      image: (
-        <span>
-          To use make it a quote, drop the link in the reply to a cast
-        </span>
-      ),
+      image: ogImage,
+      buttons: [
+        <Button action="link" target={ogImage}>
+          share
+        </Button>,
+        <Button action="link" target="https://warpcast.com/flick">
+          created by flick
+        </Button>,
+      ],
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      image: <span>error</span>,
       buttons: [
         <Button
           action="link"
@@ -30,27 +73,6 @@ const handleRequest = frames(async (ctx) => {
       ],
     };
   }
-  const { cast } = await fetchCast({
-    identifier: messageHash,
-    type: "hash",
-  });
-  const parentHash = cast?.parent_hash;
-  const ogImage = `${
-    process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000"
-  }/quote/${parentHash}`;
-  return {
-    image: ogImage,
-    buttons: [
-      <Button action="link" target={ogImage}>
-        share
-      </Button>,
-      <Button action="link" target="https://warpcast.com/flick">
-        created by flick
-      </Button>,
-    ],
-  };
 });
 
 export const GET = handleRequest;
