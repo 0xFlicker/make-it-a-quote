39e432a99a062845da605e0a81d187d1bf572fa5
flick <github@0xflick.xyz>
2024-04-25 11:43:11 -0600

    andd image metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import { fetchMetadata } from "frames.js/next";
-
-export async function generateMetadata() {
+import { Metadata } from "next";
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
   return {
-    title: "My Page",
-    // provide a full URL to your /frames endpoint
-    other: await fetchMetadata(
-      new URL(
-        "/frames",
-        process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : "http://localhost:3000",
-      ),
-    ),
+    title: "Make it a quote",
+    openGraph: {
+      images: [
+        {
+          url: new URL(
+            "/quote/0xa9c00b85171e2b9db7ccaf627fc12375e33eae6c",
+            baseUrl,
+          ).toString(),
+        },
+      ],
+    },
+    other: await fetchMetadata(new URL("/frames", baseUrl)),
   };
 }
 
