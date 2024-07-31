2e8febb51f33ff77d6735b04a7d4dedfd03c621e
flick <github@0xflick.xyz>
2024-07-30 20:24:14 -0600

    update frames add airstack

--- src/app/frames/frames.ts
+++ src/app/frames/frames.ts
@@ -1,5 +1,22 @@
 import { createFrames } from "frames.js/next";
+import { farcasterHubContext } from "frames.js/middleware";
 
 export const frames = createFrames({
   basePath: "/frames",
+  middleware: [
+    farcasterHubContext({
+      ...(process.env.NODE_ENV === "production"
+        ? {
+            hubHttpUrl: "https://hubs.airstack.xyz",
+            hubRequestOptions: {
+              headers: {
+                "x-airstack-hubs": process.env.AIRSTACK_API_KEY as string,
+              },
+            },
+          }
+        : {
+            hubHttpUrl: "http://localhost:3010/hub",
+          }),
+    }),
+  ],
 });
