1e59fa9d857e86d2798e44ba7edc930bbe6b6a68
flick <github@0xflick.xyz>
2025-04-09 21:19:05 -0600

    feat: fix mini app button

--- src/app/composer/[castId]/page.tsx
+++ src/app/composer/[castId]/page.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import { DefaultProvider } from "./default";
 import { Metadata } from "next";
 import { Client } from "./client";
+import { baseUrl } from "@/config";
 
-const frame = {
-  version: "next",
-  imageUrl: "https://quote.flick.ing/flick-full.png",
-  button: {
-    title: "Make it a quote",
-    action: {
-      type: "launch_frame",
-      url: "https://quote.flick.ing/composer/action",
-      name: "Make it a quote",
-      splashImageUrl: "https://quote.flick.ing/flick.png",
-      splashBackgroundColor: "#3D1A66",
+export async function generateMetadata({
+  params,
+}: {
+  params: { castId: string };
+}): Promise<Metadata> {
+  const frame = {
+    version: "next",
+    imageUrl: "https://quote.flick.ing/flick-full.png",
+    button: {
+      title: "Make it a quote",
+      action: {
+        type: "launch_frame",
+        url: `${baseUrl}/composer/${params.castId}`,
+        name: "Make it a quote",
+        splashImageUrl: "https://quote.flick.ing/flick.png",
+        splashBackgroundColor: "#3D1A66",
+      },
     },
-  },
-};
+  };
 
-export async function generateMetadata({}: {}): Promise<Metadata> {
   return {
     title: "Make it a quote",
     openGraph: {
